9a31ab6970b7e00de1d7b4471bedbf6fb35878c8
siyul-park <syara.park@gmail.com>
2025-10-14 22:20:15 +0900

    refactor: rename function and file

--- instr/instr.go
+++ instr/instr.go
@@ -27,6 +27,23 @@ func Marshal(instrs []Instruction) []byte {
 	return code
 }
 
+func Disassemble(code []byte) string {
+	var sb strings.Builder
+	ip := 0
+	for _, inst := range Unmarshal(code) {
+		line := fmt.Sprintf("%04d:\t", ip)
+		if inst == nil {
+			line += "<invalid>\n"
+			sb.WriteString(line)
+			break
+		}
+		line += inst.String() + "\n"
+		sb.WriteString(line)
+		ip += len(inst)
+	}
+	return sb.String()
+}
+
 func New(op Opcode, operands ...uint64) Instruction {
 	typ, ok := types[op]
 	if !ok {
