20a67bbe603ddd43bc4b49867de688573441c422
Ara Park <siyual.bak@gmail.com>
2026-07-08 00:09:54 +0900

    Add canonical program text formatter

--- program/program.go
+++ program/program.go
@@ -2,12 +2,14 @@ package program
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
-	"github.com/siyul-park/minivm/types"
-
 	"github.com/siyul-park/minivm/instr"
+	"github.com/siyul-park/minivm/types"
 )
+
+const textFormatVersion = 1
 
 type Program struct {
 	Code      []byte
@@ -55,30 +57,112 @@ func New(instrs []instr.Instruction, options ...func(*Program)) *Program {
 
 func (p *Program) String() string {
 	var sb strings.Builder
-	sb.WriteString(instr.Format(p.Code))
+	sb.WriteString(fmt.Sprintf(".version %d\n\n", textFormatVersion))
+	writeCodeSection(&sb, p.Code)
 	if len(p.Locals) > 0 {
-		sb.WriteString("\n")
+		writeSection(&sb, ".locals")
 		writeIndexed(&sb, p.Locals)
 	}
 	if len(p.Constants) > 0 {
-		sb.WriteString("\n")
-		writeIndexed(&sb, p.Constants)
+		writeSection(&sb, ".constants")
+		writeConstants(&sb, p.Constants)
 	}
 	if len(p.Types) > 0 {
-		sb.WriteString("\n")
+		writeSection(&sb, ".types")
 		writeIndexed(&sb, p.Types)
+	}
+	if len(p.Handlers) > 0 {
+		writeSection(&sb, ".handlers")
+		writeHandlers(&sb, p.Handlers)
 	}
 	return sb.String()
 }
 
+func writeCodeSection(sb *strings.Builder, code []byte) {
+	sb.WriteString(".code\n")
+	sb.WriteString(instr.Format(code))
+}
+
+func writeSection(sb *strings.Builder, name string) {
+	sb.WriteByte('\n')
+	sb.WriteString(name)
+	sb.WriteByte('\n')
+}
+
 func writeIndexed[T fmt.Stringer](sb *strings.Builder, items []T) {
 	for i, item := range items {
-		head, tail, _ := strings.Cut(item.String(), "\n")
-		sb.WriteString(fmt.Sprintf("%04d:\t%s\n", i, head))
-		for tail != "" {
-			var line string
-			line, tail, _ = strings.Cut(tail, "\n")
-			sb.WriteString(fmt.Sprintf("\t%s\n", line))
+		writeIndexedBody(sb, i, item.String())
+	}
+}
+
+func writeConstants(sb *strings.Builder, consts []types.Value) {
+	for i, c := range consts {
+		writeIndexedBody(sb, i, formatConstant(c))
+	}
+}
+
+func writeHandlers(sb *strings.Builder, handlers []instr.Handler) {
+	for i, h := range handlers {
+		body := fmt.Sprintf("start=%04d end=%04d catch=%04d depth=%d", h.Start, h.End, h.Catch, h.Depth)
+		writeIndexedBody(sb, i, body)
+	}
+}
+
+func writeIndexedBody(sb *strings.Builder, index int, body string) {
+	head, tail, _ := strings.Cut(body, "\n")
+	sb.WriteString(fmt.Sprintf("%04d:\t%s\n", index, head))
+	for tail != "" {
+		var line string
+		line, tail, _ = strings.Cut(tail, "\n")
+		if line == "" {
+			continue
 		}
+		sb.WriteString(fmt.Sprintf("\t%s\n", line))
+	}
+}
+
+func formatConstant(v types.Value) string {
+	switch v := v.(type) {
+	case types.I1:
+		return fmt.Sprintf("i1 %t", bool(v))
+	case types.I8:
+		return fmt.Sprintf("i8 %d", int8(v))
+	case types.I32:
+		return fmt.Sprintf("i32 %d", int32(v))
+	case types.I64:
+		return fmt.Sprintf("i64 %d", int64(v))
+	case types.F32:
+		return "f32 " + strconv.FormatFloat(float64(v), 'g', -1, 32)
+	case types.F64:
+		return "f64 " + strconv.FormatFloat(float64(v), 'g', -1, 64)
+	case types.Ref:
+		return fmt.Sprintf("ref %d", int32(v))
+	case types.String:
+		return "string " + strconv.Quote(string(v))
+	case types.Boxed:
+		return formatBoxedConstant(v)
+	default:
+		return v.String()
+	}
+}
+
+func formatBoxedConstant(v types.Boxed) string {
+	switch v.Kind() {
+	case types.KindI1:
+		return fmt.Sprintf("i1 %t", v.Bool())
+	case types.KindI8:
+		return fmt.Sprintf("i8 %d", v.I8())
+	case types.KindI32:
+		return fmt.Sprintf("i32 %d", v.I32())
+	case types.KindI64:
+		return fmt.Sprintf("i64 %d", v.I64())
+	case types.KindF32:
+		return "f32 " + strconv.FormatFloat(float64(v.F32()), 'g', -1, 32)
+	case types.KindF64:
+		return "f64 " + strconv.FormatFloat(v.F64(), 'g', -1, 64)
+	case types.KindRef:
+		return fmt.Sprintf("ref %d", v.Ref())
+	default:
+		return v.String()
 	}
 }
